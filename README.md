c4f2d178ea28b076ec0563979056bbafad2049a4
jamiefeiss <jamie.feiss@gmail.com>
2022-06-09 15:28:38 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# GNAF Prez Theme
+This repo is used for deploying Prez instances.
+
+## Configuration
+`config.yaml` contains all necessary options for configuring, theming & deploying an instance of Prez.
+
+## Theming
+The `theme/` folder contains the HTML, CSS, JavaScript & image files to be mounted as a volume in a container deployment.
+
+Jinja2 HTML templates go under `templates/`, (which can include `fonts.html`, which can contain `<link>` elements for fonts).
+
+## Deployment
+Prez is designed to be deployed in a containerised environment. `pipeline_script.py` will perform token replacement from values in `config.yaml` to the deployment file chosen in the `deploymentMethod` field in `config.yaml`. Currently, three deployment types are supported: Docker (docker-compose), Kubernetes & ECS.
+
+Note: Only the Kubernetes option is fully implemented.
+
+### Docker Container
+Use `docker-compose.yml`, otherwise a simple shell script `docker-run.sh` is provided to run a docker container locally.
+
+### Kubernetes
+
+- Volume
+    - Persistent volume claim
+- ConfigMap contains env vars for container
+- Ingress
+
+For AWS, a StorageClass with an EBS provisioner is recommended, provided in `deploy/kube/kube-ebs-storage-class.yaml`.
+
+### ECS
+Not yet implemented.
+
+## Pipelines
+This repo contains pipeline definitions for GitHub actions, BitBucket Pipelines & Azure Pipelines.
+
+The SPARQL credentials (`[VOC|SPACE]PREZ_SPARQL_ENDPOINT` (required), `[VOC|SPACE]PREZ_SPARQL_USERNAME` & `[VOC|SPACE]PREZ_SPARQL_PASSWORD`) should be set as repository secrets.
+
+All pipeline methods follow the same steps:
+
+- Run `pipeline_script.py` for token replacement
+- Depending on `deploymentMethod` value, deploy as necessary
+
+### GitHub Actions
+
+Currently, only the Kubernetes deployment option is supported, where EKS is used.
+
+The required repository secrets are:
+
+- `KUBE_CONFIG`
+    - Obtained by locally running:
+        ```bash
+        cat $HOME/.kube/config | base64 
+        ```
+        - Note that any AWS_PROFILEs in this config should be removed beforehand
+- `AWS_ACCESS_KEY_ID`
+- `AWS_SECRET_ACCESS_KEY`
+
+Along with the required secrets for Prez itself (`[VOC|SPACE]PREZ_SPARQL_ENDPOINT` (required), `[VOC|SPACE]PREZ_SPARQL_USERNAME` & `[VOC|SPACE]PREZ_SPARQL_PASSWORD`).
+
+### BitBucket Pipelines
+Not yet implemented.
+
+### Azure Pipelines
+Not yet implemented.
---
+++ pipeline_script.py
@@ -0,0 +1,84 @@
+import os
+import json
+import re
+
+import yaml
+
+# find & replace tokens for repo envs (sparql creds)
+VOCPREZ_SPARQL_ENDPOINT = os.environ.get("VOCPREZ_SPARQL_ENDPOINT", "")
+VOCPREZ_SPARQL_USERNAME = os.environ.get("VOCPREZ_SPARQL_USERNAME", "")
+VOCPREZ_SPARQL_PASSWORD = os.environ.get("VOCPREZ_SPARQL_PASSWORD", "")
+SPACEPREZ_SPARQL_ENDPOINT = os.environ.get("SPACEPREZ_SPARQL_ENDPOINT", "")
+SPACEPREZ_SPARQL_USERNAME = os.environ.get("SPACEPREZ_SPARQL_USERNAME", "")
+SPACEPREZ_SPARQL_PASSWORD = os.environ.get("SPACEPREZ_SPARQL_PASSWORD", "")
+
+with open("config.yaml", "r") as f:
+    c = f.read()
+c = c.replace("#{VOCPREZ_SPARQL_ENDPOINT}#", VOCPREZ_SPARQL_ENDPOINT)
+c = c.replace("#{VOCPREZ_SPARQL_USERNAME}#", VOCPREZ_SPARQL_USERNAME)
+c = c.replace("#{VOCPREZ_SPARQL_PASSWORD}#", VOCPREZ_SPARQL_PASSWORD)
+c = c.replace("#{SPACEPREZ_SPARQL_ENDPOINT}#", SPACEPREZ_SPARQL_ENDPOINT)
+c = c.replace("#{SPACEPREZ_SPARQL_USERNAME}#", SPACEPREZ_SPARQL_USERNAME)
+c = c.replace("#{SPACEPREZ_SPARQL_PASSWORD}#", SPACEPREZ_SPARQL_PASSWORD)
+
+# read config.yaml
+config = yaml.load(c, Loader=yaml.Loader)
+
+# find & replace remaining tokens depending on deployment method (& pipeline method?)
+deploy_file = ""
+deploy_str = ""
+if config["deploymentMethod"] == "kubernetes":
+    deploy_file = "kube-manifest.yaml"
+elif config["deploymentMethod"] == "ecs":
+    deploy_file = "ecs-task-def.json"
+elif config["deploymentMethod"] == "docker":
+    deploy_file = "docker-compose.yml"
+else:
+    raise ValueError(
+        "Deployment method must be one of: 'kubernetes', 'ecs' or 'docker'"
+    )
+
+
+def config_name(name: str) -> str:
+    """Transforms the camelCase config var name to UPPER_FORMAT"""
+    name_list = [word.upper() for word in re.split("(?=[A-Z])", name)]
+    return "_".join(name_list)
+
+
+def config_val(key: str) -> str:
+    """Returns the appropriate format of the config variable for each deploy file type"""
+    k = config[key]
+    deploy_type = config["deploymentMethod"]
+    if k is None:
+        return ""
+    elif isinstance(k, bool):
+        if deploy_type in ["kubernetes", "docker"]:
+            return '"' + str(k) + '"'
+        else:  # ecs
+            return str(k)
+    elif isinstance(k, int):
+        return str(k)
+    elif isinstance(k, list) or isinstance(k, dict):
+        if deploy_type in ["kubernetes", "docker"]:
+            return "'" + json.dumps(k) + "'"
+        else:  # ecs
+            return str(k)
+    else:  # str
+        return k
+
+
+with open(f"deploy/{deploy_file}", "r") as f:
+    deploy_str = f.read()
+
+# need validation for each config var
+for key in config.keys():
+    deploy_str = deploy_str.replace(f"#{{{config_name(key)}}}#", config_val(key))
+
+# remove http:// for kubernetes ingress host
+if config["deploymentMethod"] == "kubernetes":
+    old_host = re.findall("- host: (.*)", deploy_str)[0]
+    new_host = old_host.split("//")[1]
+    deploy_str = deploy_str.replace(f"- host: {old_host}", f"- host: {new_host}")
+
+with open(f"deploy/new_{deploy_file}", "w") as f:
+    f.write(deploy_str)
